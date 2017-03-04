9cb3c4e9d349519599fe9fa14860c0d9f1bcf9d3
ktrant84 <katee.trant@gmail.com>
2017-03-04 11:58:54 -0800

    Add user to stories table. Create stories controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304191436) do
+ActiveRecord::Schema.define(version: 20170304195650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stories", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.boolean  "published",  default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_stories_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,13 +40,7 @@ ActiveRecord::Schema.define(version: 20170304191436) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "stories", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.boolean  "published",  default: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
   end
 
+  add_foreign_key "stories", "users"
 end
