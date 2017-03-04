2b905961982c0f53637cb3ffaccca591a211df41
Megan Marquardt <megan@bloc.io>
2017-03-04 12:25:39 -0800

    add reaction and user reaction tables

---
+++ app/models/user_reaction.rb
@@ -0,0 +1,2 @@
+class UserReaction < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304191436) do
+ActiveRecord::Schema.define(version: 20170304201653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reactions", force: :cascade do |t|
+    t.boolean  "visible",    default: false
+    t.string   "kind"
+    t.string   "phrase"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.boolean  "published",  default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "user_reactions", force: :cascade do |t|
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "story_id"
+    t.integer  "user_id"
+    t.integer  "reaction_id"
+    t.index ["reaction_id"], name: "index_user_reactions_on_reaction_id", using: :btree
+    t.index ["story_id"], name: "index_user_reactions_on_story_id", using: :btree
+    t.index ["user_id"], name: "index_user_reactions_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,13 +57,9 @@ ActiveRecord::Schema.define(version: 20170304191436) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "stories", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.boolean  "published",  default: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
   end
 
+  add_foreign_key "user_reactions", "reactions"
+  add_foreign_key "user_reactions", "stories"
+  add_foreign_key "user_reactions", "users"
 end
