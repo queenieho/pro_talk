ec421074d092b0ff9e5dd7403e99e9427287f550
Megan Marquardt <megan@bloc.io>
2017-03-05 10:27:27 -0800

    add gender, relationship to abortion, and age range to story as possible options; add to_json for serializing data

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -1,2 +1,10 @@
 class Gender < ApplicationRecord
+
+  def to_json
+    {
+      id: id,
+      icon: "#{ kind }.png",
+      description: description
+    }
+  end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,18 @@
 class Story < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :tags
-end
+  belongs_to :age_range, optional: true
+  belongs_to :relationship_to_abortion, optional: true
+  belongs_to :gender, optional: true
 
+  def to_json
+    {
+      id: id,
+      title: title,
+      content: content,
+      age_range: age_range.to_json,
+      gender: gender.to_json,
+      relationship_to_abortion: relationship_to_abortion.to_json
+    }
+  end
+end
