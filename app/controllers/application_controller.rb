8c2250add2757bc64d23776540d28c705409e5dd
Megan Marquardt <megan@bloc.io>
2017-03-05 15:14:48 -0800

    update flow for demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    if !current_user.verified?
-      return logout_path
-    elsif current_user.username.blank?
+    if current_user.username.blank?
       return details_path
     elsif !current_user.agrees_to_code_of_conduct?
       return code_of_conduct_path
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -39,9 +39,9 @@ class RegistrationsController < Devise::RegistrationsController
   # end
 
   # # The path used after sign up.
-def after_sign_up_path_for(resource)
-  after_sign_in_path_for(resource)
-end
+  def after_sign_up_path_for(resource)
+    after_sign_in_path_for(resource)
+  end
 
   # # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
