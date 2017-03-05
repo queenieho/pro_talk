f289eb074e68a0d6f71d4d7f313c83dacb6ae6f4
ktrant84 <katee.trant@gmail.com>
2017-03-04 18:53:05 -0800

    Add logic to users controller show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,8 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find_by(id: params[:id])
+    @stories = Story.where(user_id: current_user.id)
   end
 
   def edit
