acef33360202a69cf0adc59271e0d1a447ec80e2
LauraKirby <laurakirby26@gmail.com>
2017-03-04 17:41:26 -0800

    add basic crud to user resource, add a user detail form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,55 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   protect_from_forgery prepend: true
 
-  def details
+  def index
+    @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(story_params.merge(user_id: @user))
+    if @user.save
+      redirect_to details_path
+    else
+      flash[:danger] = "Your user details did not save."
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(story_params)
+      redirect_to stories_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  def details
+    @user = current_user
+  end
+
+  private
+
+  def confirm_login
+    redirect_to login_path if current_user.nil?
+  end
+
+  def user_params
+    params.require(:user).permit(:age,:city,:gender,:religion,:state,:username,)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   end
 
   resources :stories
-
+  resources :users
   get 'details' => 'users#details'
 end
